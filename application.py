ee34babf376828e2020f9ee46508808f8fc5a7d0
Deepak S Alur <82048872+deepakalur4@users.noreply.github.com>
2024-06-20 14:51:51 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,redirect,request
+from flask import Flask,render_template,redirect,request 
 from src.pipelines.predict_pipeline import  *
 
 application=Flask(__name__)
